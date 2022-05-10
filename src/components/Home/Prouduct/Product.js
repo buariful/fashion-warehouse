5ff5102a249a0d3dd56fcd79b8c2200e7916b84e
buariful <arifulislam321054@gmail.com>
2022-05-10 15:53:49 +0600

    product section responsive

--- src/components/Home/Prouduct/Product.js
+++ src/components/Home/Prouduct/Product.js
@@ -4,10 +4,12 @@ import SingleProduct from './SingleProduct/SingleProduct';
 const Product = (props) => {
     const { name, products } = props.proData;
     return (
-        <div className='flex items-center'>
-            <h1 className='basis-1/5 font-bold text-2xl'>{name}</h1>
+        <div className='md:flex items-center'>
+            <h1 className='basis-1/5 font-bold text-2xl'>
+                <span className='font-thin text-xl'>Shiper name: </span>
+                {name}</h1>
             <div className="basis-4/5">
-                <div className='grid grid-cols-3 gap-8'>
+                <div className='grid md:grid-cols-3 sm:grid-cols-2 gap-8 mb-5'>
                     {products.map(product => <SingleProduct key={product.productId} product={product} shipper={name}></SingleProduct>)}
                 </div>
             </div>

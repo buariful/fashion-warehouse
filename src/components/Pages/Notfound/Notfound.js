3a0cecca635c443949f608c6803504c94b24ba1d
buariful <arifulislam321054@gmail.com>
2022-05-22 23:54:51 +0600

    404 page create

--- src/components/Pages/Notfound/Notfound.js
+++ src/components/Pages/Notfound/Notfound.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Notfound = () => {
     return (
         <div>
-            <img src="" alt="" />
-            <h1>Opps...! You are in the wrong place ...Go back to <Link to='/' className='text-orange-700 font-semibold'>Home</Link> </h1>
+            <h1 className='text-2xl font-bold'>Opps...! You are in the wrong place ...Go back to <Link to='/' className='text-orange-700 font-semibold'>Home</Link> </h1>
+            <img src="https://i.ibb.co/c15vngw/depositphotos-45550465-stock-photo-cocktail-dog.webp" alt="404 page img" className='mx-auto' />
         </div>
     );
 };
